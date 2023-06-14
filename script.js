d51889c90ba8da085e8a11bd2873875bf738ebef
qqaazz0222 <qqaazz0222@naver.com>
2023-06-14 22:29:11 +0900

    배포 2차

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-function view(id) {}
+function visible(id) {
+    popup = document.getElementById("popup");
+    detailImg = document.getElementById("detailImg");
+    detailImg.src = "./image/img" + id + ".png";
+    popup.style.display = "flex";
+}
+
+function invisible() {
+    popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
